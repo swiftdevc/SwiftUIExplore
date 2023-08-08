0bdc9312b9334e0a58e9efc21a10a17997f636b2
hasan cimcik <cimcikhasan06@gmail.com>
2023-08-09 01:04:52 +0300

    Initial Commit

---
+++ SwiftUIExplore/SwiftUIExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExploreApp.swift
+//  SwiftUIExplore
+//
+//  Created by Hasan PC on 9.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
